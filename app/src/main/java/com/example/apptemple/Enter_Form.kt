b39ac4880e70a5c2824418c4f952bbd5002d4626
unknown <ilya21070401@gmail.com>
2024-03-21 00:26:24 +0300

    Создание UI
    Тут я собрал примерный интерфейс входа и регистрации, сделал переход между формами и поправил Mainfest, чтобы приложение запускалось не с файла MainActivity, а с моего окна входа.

---
+++ app/src/main/java/com/example/apptemple/Enter_Form.kt
@@ -0,0 +1,29 @@
+package com.example.apptemple
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Enter_Form : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_enter_form)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toReg: Button = findViewById(R.id.button_Enter_Register)
+
+        toReg.setOnClickListener{
+            val intent = Intent(this, Register_Form::class.java)
+            startActivity(intent)
+        }
+    }
+}
