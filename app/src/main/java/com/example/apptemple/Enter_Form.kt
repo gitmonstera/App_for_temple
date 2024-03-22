391ac3929bb337fcc764f7bd9342c22c7706e027
unknown <ilya21070401@gmail.com>
2024-03-22 23:21:55 +0300

    Да
    Сделал интерфейс ближе к инстаграммному и добавил окно, где будет лента новостей

--- app/src/main/java/com/example/apptemple/Enter_Form.kt
+++ app/src/main/java/com/example/apptemple/Enter_Form.kt
@@ -3,6 +3,8 @@ package com.example.apptemple
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,12 +20,29 @@ class Enter_Form : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val userLogin: EditText = findViewById(R.id.editText_Enter_Login)
+        val userPassword: EditText = findViewById(R.id.editText_Enter_Password)
+        val buttonEnter: Button = findViewById(R.id.button_Enter_Enter)
 
+        changeWindow()
+        enterTheApp(userLogin, userPassword, buttonEnter)
+    }
+
+    fun changeWindow(){
         val toReg: Button = findViewById(R.id.button_Enter_Register)
 
         toReg.setOnClickListener{
             val intent = Intent(this, Register_Form::class.java)
             startActivity(intent)
+        }
+    }
+
+    fun enterTheApp(userLogin: EditText, userPassword: EditText, buttonEnter: Button){
+        val enteredLogin = userLogin.text.toString()
+        val enteredPassword = userPassword.text.toString()
+
+        buttonEnter.setOnClickListener{
+            //Тут условие для проверки вводимых в поля данных
         }
     }
 }
--- app/src/main/java/com/example/apptemple/News_Feed.kt
+++ app/src/main/java/com/example/apptemple/News_Feed.kt
@@ -1,31 +1,20 @@
 package com.example.apptemple
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Register_Form : AppCompatActivity() {
+class News_Feed : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_form)
+        setContentView(R.layout.activity_news_feed)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        changeWindow()
-    }
-    fun changeWindow() {
-        val toEnter: Button = findViewById(R.id.button_Register_Enter)
-
-        toEnter.setOnClickListener {
-            val intent = Intent(this, Enter_Form::class.java)
-            startActivity(intent)
         }
     }
 }
