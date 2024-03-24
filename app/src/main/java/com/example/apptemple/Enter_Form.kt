565115726401bb1a17fe9dea322e64ac3f41af87
FredNekrasov <frednekrasov49@gmail.com>
2024-03-24 17:23:03 +0300

    рефакторинг кода. Добавление привязки данных.

--- app/src/main/java/com/example/apptemple/Enter_Form.kt
+++ app/src/main/java/com/example/apptemple/Enter_Form.kt
@@ -2,46 +2,39 @@ package com.example.apptemple
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.apptemple.databinding.ActivityEnterFormBinding
 
 class Enter_Form : AppCompatActivity() {
+    private val binding by lazy { ActivityEnterFormBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_enter_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val userLogin: EditText = findViewById(R.id.editText_Enter_Login)
-        val userPassword: EditText = findViewById(R.id.editText_Enter_Password)
-        val buttonEnter: Button = findViewById(R.id.button_Enter_Enter)
-
         changeWindow()
-        enterTheApp(userLogin, userPassword, buttonEnter)
+        enterTheApp()
     }
 
-    fun changeWindow(){
-        val toReg: Button = findViewById(R.id.button_Enter_Register)
-
-        toReg.setOnClickListener{
+    private fun changeWindow(){
+        binding.buttonEnterRegister.setOnClickListener {
             val intent = Intent(this, Register_Form::class.java)
             startActivity(intent)
         }
     }
 
-    fun enterTheApp(userLogin: EditText, userPassword: EditText, buttonEnter: Button){
-        val enteredLogin = userLogin.text.toString()
-        val enteredPassword = userPassword.text.toString()
+    private fun enterTheApp() {
+        val enteredLogin = binding.editTextEnterLogin.text.toString()
+        val enteredPassword = binding.editTextEnterPassword.text.toString()
 
-        buttonEnter.setOnClickListener{
+        binding.buttonEnterEnter.setOnClickListener {
             //Тут условие для проверки вводимых в поля данных
         }
     }
