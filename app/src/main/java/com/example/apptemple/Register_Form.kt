9493e11194fd8f030b998b71fef7677e6315a48a
FredNekrasov <frednekrasov49@gmail.com>
2024-03-24 17:25:38 +0300

    Добавление привязки данных и изменение функции changeWindow

--- app/src/main/java/com/example/apptemple/Register_Form.kt
+++ app/src/main/java/com/example/apptemple/Register_Form.kt
@@ -1,31 +1,28 @@
 package com.example.apptemple
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.apptemple.databinding.ActivityRegisterFormBinding
 
 class Register_Form : AppCompatActivity() {
+    private val binding by lazy { ActivityRegisterFormBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register_form)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         changeWindow()
     }
-    fun changeWindow() {
-        val toEnter: Button = findViewById(R.id.button_Register_Enter)
-
-        toEnter.setOnClickListener {
-            val intent = Intent(this, Enter_Form::class.java)
-            startActivity(intent)
+    private fun changeWindow() {
+        binding.buttonRegisterEnter.setOnClickListener {
+            finish()
         }
     }
 }
