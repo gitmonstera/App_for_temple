2d93b69e61b26a69a60b76141591ee1d42a54b19
FredNekrasov <frednekrasov49@gmail.com>
2024-04-11 00:24:01 +0300

    создание функций расширения: навигация между активити и вывод сообщений через тост

--- app/src/main/java/com/example/apptemple/Extension.kt
+++ app/src/main/java/com/example/apptemple/Extension.kt
@@ -1,6 +1,21 @@
 package com.example.apptemple
 
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
-fun AppCompatActivity.replace(fragment: Fragment,id: Int = R.id.frameLayout) = supportFragmentManager.beginTransaction().replace(id, fragment).commit()
+//функция расширения для AppCompatActivity, которое позволяет заменить фрагмент на экране
+fun AppCompatActivity.replace(
+    fragment : Fragment, id : Int = R.id.frameLayout
+) = supportFragmentManager.beginTransaction().replace(id, fragment).commit()
+//выводит сообщение на экран
+fun Context.displayMessage(message : String) = Toast.makeText(
+    this, message, Toast.LENGTH_LONG
+).show()
+//переход на экран
+fun<T> AppCompatActivity.navigate(javaClass : Class<T>) {
+    val intent = Intent(this, javaClass)
+    startActivity(intent)
+}
