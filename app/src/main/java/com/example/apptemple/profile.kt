1d4c8dae796e06f4ab3c2353ba12c746bacac48a
FredNekrasov <frednekrasov49@gmail.com>
2024-04-15 01:38:48 +0300

    изменена навигация и реализован вывод данных пользователя на экран с помощью тоста

--- app/src/main/java/com/example/apptemple/profile.kt
+++ app/src/main/java/com/example/apptemple/profile.kt
@@ -1,36 +1,32 @@
 package com.example.apptemple
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.example.apptemple.databinding.FragmentHomeBinding
+import com.example.apptemple.data.database.SchoolDb
 import com.example.apptemple.databinding.FragmentProfileBinding
+import com.example.apptemple.utils.*
+import org.koin.android.ext.android.get
 
 class profile : Fragment() {
     private lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater : LayoutInflater, container : ViewGroup?, savedInstanceState : Bundle?
     ): View {
-
         binding = FragmentProfileBinding.inflate(inflater, container, false)
-        val view = binding.root
+        displayUserData()
         goSettings()
-        return view
+        return binding.root
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = profile()
+    private fun displayUserData() {
+        val userName = getActivityContext().intent.getStringExtra("userName")
+        get<SchoolDb>().userDao().getAllData().find { it.username == userName }?.let {
+            getActivityContext().displayMessage(it.toString())
+        } ?: getActivityContext().displayMessage(Messages.USER_NOT_FOUND)
     }
-
-    private fun goSettings(){
-        binding.buttonSettings.setOnClickListener{
-            val intent = Intent(context, SettingsActivity::class.java)
-            startActivity(intent)
+    private fun goSettings() {
+        binding.buttonSettings.setOnClickListener {
+            getActivityContext().navigate(SettingsActivity::class.java)
         }
     }
 }
