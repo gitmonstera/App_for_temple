5f750fde37262d26e07dda64f10214aef2eb2a77
unknown <ilya21070401@gmail.com>
2024-05-04 14:54:31 +0300

    Интерфейс анонса
    Добавил кнопки, но не могу сделать обработчик для пролистывания изображений, также тестово добавил обработчик нажатий для панельки анонсов(там просто уведа выводится с порядковым номером изображения)
    БАКЫТ, ПОМОГИ, Я УЖЕ 5 СУТОК НЕ МОГУ ЭТО ПОПРАВИТЬ

--- app/src/main/java/com/example/apptemple/home.kt
+++ app/src/main/java/com/example/apptemple/home.kt
@@ -2,14 +2,17 @@ package com.example.apptemple
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.apptemple.databinding.FragmentHomeBinding
 import com.example.apptemple.utils.navigate
 
 class home : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private var currentImageIndex = 0
     override fun onCreateView(
         inflater : LayoutInflater, container : ViewGroup?, savedInstanceState : Bundle?
     ): View {
@@ -31,6 +34,32 @@ class home : Fragment() {
             SlideModel(R.mipmap.justannounce5, ScaleTypes.CENTER_CROP)
         )
         binding.imageSlider.setImageList(slideImages)
+
+        binding.announceLeftButton.setOnClickListener {
+            /*if (currentImageIndex > 0) {
+                currentImageIndex--
+                // Устанавливаем новый индекс
+                binding.imageSlider.setCurrentPosition(currentImageIndex)
+            }*/
+        }
+
+        binding.announceRightButton.setOnClickListener {
+            /*if (currentImageIndex < slideImages.size - 1) {
+                currentImageIndex++
+                // Устанавливаем новый индекс
+                binding.imageSlider.setCurrentPosition(currentImageIndex)
+            }*/
+        }
+
+        binding.imageSlider.setItemClickListener(object : ItemClickListener {
+            override fun onItemSelected(position: Int) {
+                Toast.makeText(context, "Вы нажали на ${position + 1} изображение", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun doubleClick(position: Int) {
+                // Обработка двойного нажатия (если необходимо)
+            }
+        })
     }
     private fun goSettings() {
         binding.buttonSettings.setOnClickListener {
