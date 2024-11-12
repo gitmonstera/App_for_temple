8074f48d3d33beeca44751b1b1c0828d649f2869
GreeiLLL <ilya21070401@gmail.com>
2024-11-12 11:53:32 +0300

    1. Сделал дата классы для анонсов и новостей 2. Сделал адаптеры для дата классов и адаптеров 3. Сделал анимацию для скрывания анонсов 4. Пересобрал модель работы с лентой новостей 5. Добавил обработчик нажатий на элементы слайдера анонсов 6. В окне настроек переделал кнопки на лэйауты для кастомизации 7. Добавил кнопку перехода в телеграм 8. Добавил OkHttp для приема ответов от сервера

--- app/src/main/java/com/example/apptemple/QuestionActivity.kt
+++ app/src/main/java/com/example/apptemple/QuestionActivity.kt
@@ -1,18 +1,17 @@
 package com.example.apptemple
 
+import androidx.appcompat.app.AppCompatDelegate
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.apptemple.databinding.ActivityScheduleBinding
+import com.example.apptemple.databinding.ActivityQuestionBinding
 
-class Schedule_Activity : AppCompatActivity() {
-    private lateinit var binding: ActivityScheduleBinding
+class QuestionActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityQuestionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityScheduleBinding.inflate(layoutInflater)
+        binding = ActivityQuestionBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         goBack()
     }
