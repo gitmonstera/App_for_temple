8b0f628249a66d0be370304187bec05d303de5fa
GreeiLLL <ilya21070401@gmail.com>
2024-11-22 00:18:08 +0300

    Элемент секций и лента анонсов 1. Сделал пять постов из ресурсов для теста 2. Сделал окно с вопросами через Linear

--- app/src/main/java/com/example/apptemple/DataClasses/NewsData.kt
+++ app/src/main/java/com/example/apptemple/DataClasses/NewsData.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class NewsData(
     @SerializedName("title") val newsTitle: String,
     @SerializedName("content") val newsDescription: String,
-    @SerializedName("photo_url") val newsImage: String
+    @SerializedName("photo_url") val newsImage: Int
 )
